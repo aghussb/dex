f919918e329aae95e2b6d6ed9c469b5734682507
feng <f3n67u@outlook.com>
2021-09-18 23:53:06 +0800

    chore: update @jspm/core to 2.0.0-beta.11

---
+++ lib/deps/@jspm/core@2.0.0-beta.11/nodelibs/deno/console.ts
@@ -0,0 +1,2 @@
+export * from 'https://deno.land/std@0.106.0/node/console.ts';
+export { default } from 'https://deno.land/std@0.106.0/node/console.ts';
---
+++ lib/deps/@jspm/core@2.0.0-beta.11/nodelibs/deno/stream.ts
@@ -0,0 +1,2 @@
+export * from 'https://deno.land/std@0.106.0/node/stream.ts';
+export { default } from 'https://deno.land/std@0.106.0/node/stream.ts';
